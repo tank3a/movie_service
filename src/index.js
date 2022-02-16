5d3afce2b7b321f9de6c44ec5cf9f93f1dd50efc
tank3a <tank3a@gmail.com>
2022-02-16 18:27:03 +0900

    error correction

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router basename={process.env.PUBLIC_URL}>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
