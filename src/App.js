27a56acae1068323984d6799e8da2d2d33219ddf
tank3a <tank3a@gmail.com>
2022-02-17 13:29:49 +0900

    Path modified

--- src/App.js
+++ src/App.js
@@ -6,12 +6,8 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route basename={process.env.PUBLIC_URL} path="/movie/:id">
-          <Detail />
-        </Route>
-        <Route basename={process.env.PUBLIC_URL} path="/">
-          <Home />
-        </Route>
+        <Route path={process.env.PUBLIC_URL + "/movie/:id"} element={<Detail />} />
+        <Route path={process.env.PUBLIC_URL + "/"} element={<Home />} />
       </Switch>
     </Router>
   )
