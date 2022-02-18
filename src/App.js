2ddd3df72a90f7d5d5fe778ff827fb154566c94e
tank3a <tank3a@gmail.com>
2022-02-18 16:53:19 +0900

    Divided routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import Home from "./routes/Home";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Detail from "./routes/Detail";
+import Coin from "./routes/Coin";
+import ToDoList from "./routes/ToDoList";
+import MovieApp from "./routes/MovieApp";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="movie/:id">
+        <Route path="/movie/:id">
           <Detail />
         </Route>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={Home} >
-          <Home />
+        <Route path="/coins">
+          <Coin />
+        </Route>
+        <Route path="todos">
+          <ToDoList />
+        </Route>
+        <Route path="/" >
+          <MovieApp />
         </Route>
       </Switch>
     </Router>
--- src/routes/MovieApp.js
+++ src/routes/MovieApp.js
@@ -1,4 +1,4 @@
-import Movie from "./Movie";
+import Movie from "../components/Movie";
 import { useState, useEffect } from "react";
 
 function MovieApp() {
